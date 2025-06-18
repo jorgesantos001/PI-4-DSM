b04d4e5af217d5910cbbaf290f4c4dbfdb5f8eca
Jorge Patrocínio <141787356+jorgesantos001@users.noreply.github.com>
2025-06-17 23:15:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -112,6 +112,6 @@ app.post('/login', async (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
